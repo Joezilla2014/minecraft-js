05feeae495a7a23ec09b6cd96284b4dc4f51233c
Joezilla2014 <63375064+Joezilla2014@users.noreply.github.com>
2020-06-05 13:11:54 -0400

    Create c.js

---
+++ src/com/mojang/minecraft/a/a/c.js
@@ -0,0 +1,16 @@
+
+package com.mojang.minecraft.a.a;
+
+public class c
+{
+    public byte[] a;
+    public int b;
+    
+    public c(final int b) {
+        this.a = new byte[1024];
+        this.b = b;
+    }
+    
+    public void a() {
+    }
+}
